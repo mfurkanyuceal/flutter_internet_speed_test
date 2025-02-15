92aa5e5f940ac9036112d9fd9172d11d9c944cd0
Muhammed Furkan Yüceal <furkanyuceal58@gmail.com>
2025-02-15 18:10:57 +0300

    feat: Enhance enum functionality with getter methods and string representation
    - Added getter methods to TestType, CallbacksEnum, ListenerEnum, and SpeedUnit enums
    - Implemented toString() method for TestType and SpeedUnit to capitalize first letter
    - Imported string_extensions for capitalizeFirst() method

--- lib/src/callbacks_enum.dart
+++ lib/src/callbacks_enum.dart
@@ -1,21 +1,53 @@
+import 'package:flutter_internet_speed_test/src/extensions/string_extensions.dart';
+
 enum TestType {
   download,
-  upload,
+  upload;
+
+  bool get isDownload => this == TestType.download;
+
+  bool get isUpload => this == TestType.upload;
+
+  @override
+  String toString() {
+    return name.capitalizeFirst();
+  }
 }
 
 enum CallbacksEnum {
   startDownLoadTesting,
-  startUploadTesting,
+  startUploadTesting;
+
+  bool get isStartDownLoadTesting => this == CallbacksEnum.startDownLoadTesting;
+
+  bool get isStartUploadTesting => this == CallbacksEnum.startUploadTesting;
 }
 
 enum ListenerEnum {
   complete,
   error,
   progress,
-  cancel,
+  cancel;
+
+  bool get isComplete => this == ListenerEnum.complete;
+
+  bool get isError => this == ListenerEnum.error;
+
+  bool get isProgress => this == ListenerEnum.progress;
+
+  bool get isCancel => this == ListenerEnum.cancel;
 }
 
 enum SpeedUnit {
   kbps,
-  mbps,
+  mbps;
+
+  bool get isKbps => this == SpeedUnit.kbps;
+
+  bool get isMbps => this == SpeedUnit.mbps;
+
+  @override
+  String toString() {
+    return name.capitalizeFirst();
+  }
 }
