8279a01cfa110310887a882512f9e5e32fabf636
Muhammed Furkan Yüceal <furkanyuceal58@gmail.com>
2025-02-15 17:54:27 +0300

    chore: Update dependencies and connectivity check logic
    - Upgraded package dependencies to latest versions
    - Simplified internet connectivity check in `speed_test_utils.dart`
    - Updated plugin_platform_interface, connectivity_plus, http, logger, and flutter_lints

--- lib/src/speed_test_utils.dart
+++ lib/src/speed_test_utils.dart
@@ -3,7 +3,6 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 Future<bool> isInternetAvailable() async {
   final connectivity = Connectivity();
   final connectivityResult = await connectivity.checkConnectivity();
-  return connectivityResult == ConnectivityResult.mobile ||
-      connectivityResult == ConnectivityResult.wifi || 
-      connectivityResult == ConnectivityResult.ethernet;
+  return !connectivityResult
+      .any((element) => element == ConnectivityResult.none);
 }
